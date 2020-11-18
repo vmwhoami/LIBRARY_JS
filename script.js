6416c1fab323be303507f605816627c632dd3a01
Georjane <pzgeorjane@gmail.com>
2020-11-18 14:12:31 +0100

    Create add book and delete book button

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const container = document.querySelector(".container")
 
+const submitbtn = document.querySelector('.submit')
+const title = document.querySelector('#title')
+const author = document.querySelector('#author')
+const page = document.querySelector('#page')
+const price = document.querySelector('#price')
+const read = document.querySelector('#read')
 
 
 
@@ -15,8 +21,14 @@ function Book(title,author,pages,price,read) {
    this.price = price,
    this.read = read
 }
+
+
 function addBookToLibrary(input) {
   myLibrary.push(input)
+}
+
+function removeBookFromLibrary(index) {
+  myLibrary.splice(index, 1)
 }
 
 let book1 = new Book("The Boook","Hemingway",200,20,true)
@@ -25,7 +37,19 @@ let book3 = new Book("Second Book","Wya",300,10,false)
 addBookToLibrary(book1)
 addBookToLibrary(book2)
 addBookToLibrary(book3)
- 
+
+
+displayLibrary(myLibrary)
+
+
+submitbtn.addEventListener("click",function(e){
+  e.preventDefault()
+  let newbook = new Book(title.value,author.value, page.value, price.value, read.value)
+  addBookToLibrary(newbook)
+  // displayLibrary(myLibrary)
+})
+
+
 
 function displayLibrary(array){
   for(let i = 0; i<array.length; i++){
@@ -39,9 +63,19 @@ function displayLibrary(array){
     description.textContent = `This book is written ${book.author} and it has ${book.pages} pages and it
     costs ${book.price}$
     `
+    const deleteBookBtn = document.createElement('button')
+    deleteBookBtn.textContent = "Delete book"
+    deleteBookBtn.setAttribute('data-attribute', i)
+
+    deleteBookBtn.addEventListener("click",function(e){
+      const index = e.target.getAttribute('data-attribute')
+      removeBookFromLibrary(index)
+    })
+
+    
     div.appendChild(title)
     div.appendChild(description)
-
+    div.appendChild(deleteBookBtn)
   }
 }
 
@@ -53,8 +87,6 @@ btn.addEventListener("click",function(e){
 })
 
 
+// console.log(myLibrary);
 
-console.log(myLibrary);
-
-displayLibrary(myLibrary)
 
